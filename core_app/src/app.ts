2c158817df856443be57df94355a99bc17f6f514
jungai <juworaphol125@gmail.com>
2021-10-27 14:10:04 +0700

    refactor: :zap: improve logging

--- core_app/src/app.ts
+++ core_app/src/app.ts
@@ -1,10 +1,25 @@
 import Fastify from 'fastify';
 
 export const fastify = Fastify({
-    logger: true,
+    logger: {
+        prettyPrint: true,
+        serializers: {
+            req(request) {
+                return {
+                    requestId: request.id,
+                    method: request.method,
+                    url: request.url,
+                    parameters: request.params,
+                };
+            },
+            // res use default default
+        },
+    },
 });
 
 // Declare a route
-fastify.get('/', function (request, reply) {
+fastify.get('/', function (_request, reply) {
+    // console.log(`req -> ${request?.user as any}`);
+    // throw new Error('eiei');
     reply.send({ hello: 'world' });
 });
